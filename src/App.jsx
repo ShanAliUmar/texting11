a8e87cc50af8a4618d58584c47743d026537cf48
ShanAliUmar <shanumar.dev@gmail.com>
2023-11-15 08:46:12 +0500

    sahkj

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+// import "./style.scss"
+
+const App = () => {
+  return (
+    <>
+      <h1>
+        shan
+        <div className="logo">shan</div>
+      </h1>
+    </>
+  )
+}
+
+
+export default App
